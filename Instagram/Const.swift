b14569c04d0425024e3a7d897d7d1bfb870880a1
kazuma-fujita-genova <kazuma.fujita@genova.co.jp>
2018-10-10 19:01:24 +0900

    Lesson 9.3まで実装

---
+++ Instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by 藤田和磨 on 2018/10/10.
+//  Copyright © 2018年 藤田和磨. All rights reserved.
+//
+
+import Foundation
+
+struct Const {
+    static let PostPath = "posts"
+}
